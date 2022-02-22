2f647b0bdcbc473281011de8f436c11f668a18db
genie0806 <genie0806@gmail.com>
2022-02-22 23:30:40 +0900

    라이브러리 추가, API 받아오는 부분 추가

---
+++ lib/data_remote/pixabay_api.dart
@@ -0,0 +1,15 @@
+import 'package:staggered_gridview_test/model/search_model.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class PixaBayApi {
+  static const baseUrl = 'https://pixabay.com/api/';
+
+  Future<SearchModel> fetchSearchData(String searchKeyword) async {
+    final response = await http.get(Uri.parse(
+        '$baseUrl?key=17828481-17c071c7f8eadf406822fada3&q=$searchKeyword&image_type=photo'));
+    Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
+    SearchModel searchData = SearchModel.fromJson(jsonResponse);
+    return searchData;
+  }
+}
